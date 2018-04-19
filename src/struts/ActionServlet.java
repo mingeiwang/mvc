0dba3e72041784555c9acde026d7b9a83b4cfe5d
mingeiwang <875345959@qq.com>
2018-04-19 10:04:47 +0800

    首次提交

---
+++ src/struts/ActionServlet.java
@@ -0,0 +1,62 @@
+package struts;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ActionServlet
+ */
+@WebServlet("/ActionServlet")
+public class ActionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		request.setAttribute("x", name);
+		String url = null;
+		if(isUser(name)){
+			request.setAttribute("mess", "成功");
+			url = "/view/myJsp.jsp";
+		} else {
+			request.setAttribute("mess", "失败");
+			url = "/view/error.jsp";
+		}
+		RequestDispatcher dis = request.getRequestDispatcher(url);
+		dis.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	private boolean isUser(String user){
+		boolean b = false;
+		if("xyk".equals(user)){
+			b = true;
+		}
+		return b;
+	}
+
+}
